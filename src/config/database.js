75e757cd90ad314cefda3e67b8aac48a780b2417
Luan <luanitachi@hotmail.com>
2024-06-07 15:16:27 -0300

    att

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-  url: 'postgres://luan:a4GPJQZ56jqBcGZHeoCPr2TktNCjTcsV@dpg-cphkidsf7o1s739kfhg0-a.oregon-postgres.render.com/codeburger_014y',
+  url: 'postgres://luan:a4GPJQZ56jqBcGZHeoCPr2TktNCjTcsV@dpg-cphkidsf7o1s739kfhg0-a/codeburger_014y',
   define: {
     timestamps: true,
     underscored: true,
