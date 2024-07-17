7bc7cd241d49885accd3ef04327d70a745e9723a
Luan <luanitachi@hotmail.com>
2024-07-16 22:53:43 -0300

    att migrations

--- src/database/migrations/20240716232532-create-users-table.cjs
+++ src/database/migrations/20240716232532-create-users-table.cjs
@@ -5,7 +5,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
 
 
-    await queryInterface.createTable('users', {
+    await queryInterface.createTable('Users', {
       id: {
         primaryKey: true,
         allowNull: false,
@@ -29,11 +29,11 @@ module.exports = {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
       },
-      created_at: {
+      createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      updated_at: {
+      updatedAt: {
         type: Sequelize.DATE,
         allowNull: false,
       }
@@ -44,7 +44,7 @@ module.exports = {
   async down(queryInterface, Sequelize) {
 
 
-    await queryInterface.dropTable('users');
+    await queryInterface.dropTable('Users');
 
   }
 };
