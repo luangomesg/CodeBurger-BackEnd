dcb4d2497cee30049f829a61f61107544bc68ffd
Luan <luanitachi@hotmail.com>
2024-07-17 14:57:19 -0300

    att t

--- src/routes.js
+++ src/routes.js
@@ -15,17 +15,20 @@ const routes = new Router()
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
+routes.post('/products', upload.single('file'), ProductController.store)
+routes.put('/products/:id', upload.single('file'), ProductController.update)
+routes.post('/categories', upload.single('file'), CategoryController.store)
+routes.put('/categories/:id', upload.single('file'), CategoryController.update)
 
 routes.use(authMiddleware)
 
-routes.post('/products', upload.single('file'), ProductController.store)
+
 routes.get('/products', ProductController.index)
-routes.put('/products/:id', upload.single('file'), ProductController.update)
 routes.delete('/products/:id', ProductController.delete)
 
-routes.post('/categories', upload.single('file'), CategoryController.store)
+
 routes.get('/categories', CategoryController.index)
-routes.put('/categories/:id', upload.single('file'), CategoryController.update)
+
 
 routes.post('/orders', OrderController.store)
 routes.get('/orders', OrderController.index)
