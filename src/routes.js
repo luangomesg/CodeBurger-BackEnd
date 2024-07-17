3adc38dc9b0aec06699b9822b4e4784ab1a12990
Luan <luanitachi@hotmail.com>
2024-07-17 15:30:02 -0300

    att t3

--- src/routes.js
+++ src/routes.js
@@ -11,27 +11,23 @@ import OrderController from './app/controllers/OrderController.js';
 const upload = multer(multerConfig);
 const routes = new Router();
 
-// Rota pública para criar usuário
+// Rotas públicas
 routes.post('/users', UserController.store);
-
-// Rota pública para autenticar usuário
 routes.post('/sessions', SessionController.store);
 
-// Rotas para manipulação de produtos (upload de imagem protegido pelo multer)
+// Middleware de autenticação aplicado a partir deste ponto
+routes.use(authMiddleware);
+
+// Rotas protegidas
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index);
 routes.put('/products/:id', upload.single('file'), ProductController.update);
 routes.delete('/products/:id', ProductController.delete);
 
-// Rotas para manipulação de categorias (upload de imagem protegido pelo multer)
 routes.post('/categories', upload.single('file'), CategoryController.store);
 routes.get('/categories', CategoryController.index);
 routes.put('/categories/:id', upload.single('file'), CategoryController.update);
 
-// Middleware de autenticação aplicado a partir deste ponto
-routes.use(authMiddleware);
-
-// Rotas protegidas para manipulação de pedidos
 routes.post('/orders', OrderController.store);
 routes.get('/orders', OrderController.index);
 routes.put('/orders/:id', OrderController.update);
