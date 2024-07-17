f8fd7f5b1b7763b6121c4b002e214a66397a6f66
Luan <luanitachi@hotmail.com>
2024-07-16 22:33:27 -0300

    att

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,17 @@
 const configDatabase = {
   dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  database: 'codeburger',
-  username: 'postgres',
-  password: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Para ambientes de desenvolvimento, pode ser necessário desativar a verificação de certificado
+    }
+  },
+  url: 'postgresql://luan:OKeO7ZZZxbTq9gRGgIqXIn1Tc0zYd91x@dpg-cqbhikggph6c73c0j1sg-a.oregon-postgres.render.com/codeburger_1m2g',
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true
-  },
-}
+  }
+};
 
-export default configDatabase
+export default configDatabase;
